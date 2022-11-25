504f2899605ba0ed387f82c5387f113dcedadbc9
Camilla Devasini <camilladevasini@gmail.com>
2022-11-25 15:14:32 +0100

    add an API route

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,3 +1,12 @@
-test("test", () => {
-    expect(true).toBe(true);
+import app from "./app";
+import supertest from "supertest";
+
+const request = supertest(app);
+test("GET /planets", async () => {
+    const response = await request
+        .get("/planets")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+
+    expect(response.body).toEqual([{ name: "Mercury" }, { name: "Venus" }]);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,6 @@
-import express from "express";
-import "express-async-errors";
+import app from "./app";
 
-const app = express();
 const port = 3000;
-
-app.get("/", (request, response) => {
-    response.send("up and running! Modified");
-});
 
 app.listen(port, () => {
     console.log(`Server runs at https://localhost:${port}`);
